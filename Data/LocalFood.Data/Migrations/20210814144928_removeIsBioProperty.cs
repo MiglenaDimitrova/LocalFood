526e09424a9e1e97792972c8c0337a08d30a0977
Miglena Dimitrova <mimas4emn@gmail.com>
2021-08-15 16:31:17 +0300

    Add image on Index page, fix MyProducts

---
+++ Data/LocalFood.Data/Migrations/20210814144928_removeIsBioProperty.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalFood.Data.Migrations
+{
+    public partial class removeIsBioProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBio",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBio",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
